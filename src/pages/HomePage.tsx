2cc974db7d5cae763b0f39a032ba9336694f2607
Abdur Rafay <rafay.mail2004@gmail.com>
2026-01-24 04:36:29 +0600

    animaiton, cta

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import { HeroSection } from '../components/home/HeroSection';
-import { StatisticsBar } from '../components/home/StatisticsBar';
 import { TrendingSection } from '../components/home/TrendingSection';
 import { CategorySection } from '../components/home/CategorySection';
 import { FeaturedArtisan } from '../components/home/FeaturedArtisan';
@@ -9,7 +8,6 @@ export function HomePage() {
     return (
         <>
             <HeroSection />
-            <StatisticsBar />
             <TrendingSection />
             <CategorySection />
             <FeaturedArtisan />
